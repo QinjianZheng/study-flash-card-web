4934119e7070fc95fc97a145afb425b523a37548
Qinjian Zheng <zhengqinjian96@gmail.com>
2023-02-25 13:19:13 +1100

    ran prettier over all files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'sfc-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'study-flash-card-web';
