ee6c3f2ba3252e92e16c1dc8dc31fcf9bf98e67a
Qinjian Zheng <zhengqinjian96@gmail.com>
2023-02-25 12:50:39 +1100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sfc-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'study-flash-card-web';
+}
